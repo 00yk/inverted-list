d4d3345bc3d74307bade76e516a87ad09d963916
Yingkai Sun <ys3988@nyu.edu>
2021-10-15 00:24:54 +0800

    Finish README

--- src/main.rs
+++ src/main.rs
@@ -515,19 +515,19 @@ where
 fn main() {
 
     parse("big.trec");
-    // let mut fvec = Vec::new();
-    // for entry in glob("./*.intermediate").expect("Failed to glob pattern") {
-    //     match entry {
-    //         Ok(path) => {
-    //             // println!("{:?}", path.display());
-    //             if let Some(filename) = path.to_str() {
-    //                 fvec.push(filename.to_string());
-    //             }
-    //         }
-    //         Err(e) => println!("{:?}", e),
-    //     }
-    // }
-    // k_way_merge(fvec);
-    // build_inverted_index_and_lexicon();
+    let mut fvec = Vec::new();
+    for entry in glob("./*.intermediate").expect("Failed to glob pattern") {
+        match entry {
+            Ok(path) => {
+                // println!("{:?}", path.display());
+                if let Some(filename) = path.to_str() {
+                    fvec.push(filename.to_string());
+                }
+            }
+            Err(e) => println!("{:?}", e),
+        }
+    }
+    k_way_merge(fvec);
+    build_inverted_index_and_lexicon();
 
 }
